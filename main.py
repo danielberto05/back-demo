159d5fb6ec1c6c2ebc7d2b59211e10053f1add2b
Daniel Farias <danielberto05@gmail.com>
2023-05-25 22:33:36 -0300

    Add Cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -7,6 +8,15 @@ from api.v1.api import api_router
 app = FastAPI(
     title="Back-end Demo",
     openapi_url="/api/v1/openapi.json",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
